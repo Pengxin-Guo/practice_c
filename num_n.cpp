218bfc17da4c1cded7790443828a866412ceaa8d
gpx <1457495424@qq.com>
2018-09-13 18:59:56 +0800

    modify num_n.cpp and add some notes

--- num_n.cpp
+++ num_n.cpp
@@ -11,11 +11,17 @@
 
 int main() {
     int n;
-    //FILE *file = fopen("/dev/null", "w");
-    char buffer[100];
+    FILE *fout = fopen("/dev/null", "w");
+    char str[100];
     while (scanf("%d", &n) != EOF) {
-        //printf("%d\n",fprintf(file, "%d", n));
-        printf("%d\n", sprintf(buffer, "%d", n));
+        printf("%d\n", fprintf(fout, "%d", n));     //输出数字n的位数
+        printf("%d\n", sprintf(str, "%d", n));      //输出数字n的位数
+
+        //sprintf的另一种用法
+        for (int i = 1, j = 0; i <= 20; i++) {
+            j += sprintf(str + j, "%d", i);         //将1到20拼接到数组str中
+        }
+        printf("str = %s\n", str);
     }
     return 0;
 }
