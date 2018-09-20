6989d6a6d17d77c23b9d3d9a859b232528b4a5f5
gpx <1457495424@qq.com>
2018-09-20 19:46:28 +0800

    modify var_para.c

--- var_para.c
+++ var_para.c
@@ -8,6 +8,11 @@
 //实现可变参数max_int，从若干个传入的参数中返回最大值。
 //int max_int(int n, ...);
 
+//获得n往后的参数列表： va_list 类型的变量
+//定位n后面第一个参数的位置：va_start函数
+//获取下一个可变参数列表中的参数：va_arg函数
+//结束整个获取可变参数列表的动作：va_end函数
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
