2b1ed61561fc95ca6d40c6d3db3b47efb46b60be
gpx <1457495424@qq.com>
2018-09-21 11:14:40 +0800

    add my_printf.c

---
+++ my_printf.c
@@ -0,0 +1,75 @@
+/*************************************************************************
+> File Name: my_printf.c
+> Author: gpx
+> Mail: 1457495424@qq.com
+> Created Time: 2018年09月20日 星期四 20时43分38秒
+************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <inttypes.h>
+#include <math.h>
+
+int digit(int n) {
+    return floor(log10(n)) + 1;
+}
+
+
+int my_printf(const char *frm, ...) {
+    int cnt = 0;
+    va_list arg;
+    va_start(arg, frm);
+    for (int i = 0; frm[i]; i++) {
+        switch (frm[i]) {
+            case '%' : {
+                i++;
+                switch(frm[i]) {
+                    case 'd' : {
+                        int temp = va_arg(arg, int);
+                        if (temp < 0) {
+                            putchar('-');
+                            cnt++;
+                            if (temp == -temp) {
+                                putchar('X');
+                                break;
+                            }
+                            temp = -temp;
+                        }
+                        int dig_temp = digit(temp);
+                        while (temp) {
+                            int num = temp / (int)pow(10, dig_temp - 1);
+                            putchar(num + '0');
+                            cnt++;
+                            temp = temp % (int)pow(10, dig_temp - 1);
+                            dig_temp -= 1;
+                        }
+                        for (int i = 0; i < dig_temp; i++) {
+                            putchar('0');
+                            cnt++;
+                        }
+                        break;
+                    } 
+                    default : 
+                    fprintf(stderr, "error : unknow %%%c\n", frm[i]); 
+                    exit(1);
+                }
+            } break;
+            default:
+            putchar(frm[i]);
+            cnt++;
+        }
+    }
+    return cnt;
+}
+
+int main() {
+    int n = 123;
+    my_printf("%d\n", my_printf("hello world\n"));
+    my_printf("%d\n", my_printf("n = %d\n", n));
+    my_printf("%d\n", my_printf("n = %d\n", 12000));
+    my_printf("%d\n", my_printf("n = %d\n", -567));
+    //my_printf("%d\n", my_printf("n = %d\n", INT32_MIN));
+    my_printf("%d\n", my_printf("n = %d\n", INT32_MAX));
+    return 0;
+}
