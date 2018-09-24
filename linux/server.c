7c14a6d83f58d91aa1e2417826ad506f890c8776
gpx <1457495424@qq.com>
2018-09-24 16:51:47 +0800

    add server.c

---
+++ linux/server.c
@@ -0,0 +1,56 @@
+/*************************************************************************
+> File Name: server.c
+> Author: gpx
+> Mail: 1457495424@qq.com
+> Created Time: 2018年09月24日 星期一 11时13分19秒
+************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "./head.h"
+
+#define PORT 33333                                                                                //端口号
+
+int main() {
+    int a = 0;
+	int server_listen, socketfd;
+    struct sockaddr_in my_addr;
+    if ((server_listen = socket(AF_INET,SOCK_STREAM,0)) < 0) {                                    //建立socket
+        perror("socket error");
+        exit(1);
+    }
+    my_addr.sin_family = AF_INET;                                                                 //该属性表示接收本机或其他机器传输
+	my_addr.sin_port = htons(PORT);                                                               //端口号，转化为网络字节序
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);                                                  //IP，括号内容表示本机IP
+	bzero(&(my_addr.sin_zero), sizeof(my_addr));                                                  //将其他属性置0
+    if (bind(server_listen, (struct sockaddr*)&my_addr, sizeof(struct sockaddr)) < 0) {           //绑定地址结构体和socket
+		perror("bind error");
+		return -1;
+	}
+    if (listen(server_listen, 20) < 0) {                                                          //开启监听，第二个参数是最大监听数
+        perror("listen error");
+        exit(0);
+    }                                       
+    while (1) {
+        struct sockaddr_in client_addr;                                                           //客户端socket
+        socklen_t len = sizeof(client_addr);
+        if ((socketfd = accept(server_listen, (struct sockaddr*)&client_addr, &len)) < 0) {
+            perror("accept error");
+            break;
+        }
+        bzero(&client_addr, len);
+        //getpeername(socketfd, (struct sockaddr *)&client_addr, &len);
+        char client_mess[20] = {'\0'};
+        inet_ntop(AF_INET, (void *)&client_addr.sin_addr, client_mess, 63);
+        char message[MAX_SIZE];
+        while ((a = recv(socketfd, message, MAX_SIZE, 0)) > 0) {
+            printf("%s:%d : recv %d 字节 %s\n", client_mess, ntohs(client_addr.sin_port), a, message);
+            fflush(stdout);
+            memset(message, 0, sizeof(message));
+        }
+        printf("\n");
+        close(socketfd);
+    }
+    close(server_listen);
+	return 0;
+}
