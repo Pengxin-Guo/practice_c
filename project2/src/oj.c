5d58e89026c4bda49e8c26a5c298364c6e6a4aee
gpx <1457495424@qq.com>
2018-10-05 11:36:25 +0800

    add some notes in oj.c

--- project2/src/oj.c
+++ project2/src/oj.c
@@ -14,16 +14,16 @@ int main()
     // Input your code here.
     char c1, c2;
     while (c1 = fgetc(fpright), c2 = getc(fpout), c1 != EOF && c2 != EOF && c1 == c2) ;
-    if (c1 != EOF && c2 != EOF) {               //c1,c2都没读到文件尾，说明出现了不一样的字符
+    if (c1 != EOF && c2 != EOF) {                                           //c1,c2都没读到文件尾，说明出现了不一样的字符
         printf("Wrong Answer\n");
-    } else if (c1 == c2) {    //此时是c1 == EOF && c2 == EOF,都读到了文件尾，说明两个文件内容一样
+    } else if (c1 == c2) {                                                  //此时是c1 == EOF && c2 == EOF,都读到了文件尾，说明两个文件内容一样
         printf("Accept\n");
-    } else {
-        if (c1 != '\n' && c2 != '\n') {  //一个文件读完后，另一个文件出现了换行之外的其他字符
+    } else {                                                                //c1与c2有一个读到了文件尾
+        if (c1 != '\n' && c2 != '\n') {                                     //一个文件读完后，另一个文件出现了换行之外的其他字符
             printf("Wrong Answer\n");
         } else {
-            c1 == EOF ? (c2 = fgetc(fpout)) : (c1 = getc(fpright));
-            printf("%s\n", c1 == c2 ? "Accept" : "Wrong Answer");
+            c1 == EOF ? (c2 = fgetc(fpout)) : (c1 = getc(fpright));         //吃掉多出来的一个换行
+            printf("%s\n", c1 == c2 ? "Accept" : "Wrong Answer");           //c1与c2是否都读到了文件尾
         }
     }
     fclose(fpout);
