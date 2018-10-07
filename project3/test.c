aed177b6d58cc0d98486daaa25bd212544ad536f
gpx <1457495424@qq.com>
2018-10-07 15:57:15 +0800

    add project3

--- project3/test.c
+++ project3/test.c
@@ -4,35 +4,45 @@
 	> Mail: 1457495424@qq.com
 	> Created Time: 2018年10月05日 星期五 15时16分21秒
  ************************************************************************/
-
 #include <stdio.h>
 #include "test.h"
 
-int add(int a, int b) {
-    return a + b;
+static struct FuncData *FuncData_head = NULL;
+
+void addFuncData(const char *a, const char *b, test_func_t func) {
+    struct FuncData *p = (struct FuncData *)malloc(sizeof(struct FuncData));
+    p->a_str = a;
+    p->b_str = b;
+    p->func = func;
+    p->next = FuncData_head;
+    FuncData_head = p;
+    return ;
 }
 
-int is_prime(int x) {
-    if (x <= 1) return 0;
-    for (int i = 2; i * i <= x; i++) {
-        if (x % i == 0) return 0;
+int RUN_ALL_TEST() {
+    struct FuncData *current_node, *next_node;
+    if (FuncData_head == NULL) return 0;
+    current_node = FuncData_head->next;
+    FuncData_head->next = NULL;
+    while (current_node != NULL) {
+        next_node = current_node->next;
+        current_node->next = FuncData_head;
+        FuncData_head = current_node;
+        current_node = next_node;
     }
-    return 1;
-}
-
-TEST(test, is_prime_func) {
-    EXPECT(is_prime(2), 0);
-    EXPECT(is_prime(-2), 0);
-    EXPECT(is_prime(15), 0);
-    EXPECT(is_prime(9973), 1);
-}
-
-TEST(test, add_func) {
-    EXPECT(add(1, 2), 3);
-    EXPECT(add(3, 4), 7);
-    EXPECT(add(2, 2), 4);
-}
-
-int main() {
-    return RUN_ALL_TEST();
+    char color[3][100] = {
+        "[ \033[1;32m%.2lf%%\033[0m ] total : %d，expect : %d\n",
+        "[ \033[0;31m%.2lf%%\033[0m ] total : %d，expect : %d\n",
+        "[ \033[1;31m%.2lf%%\033[0m ] total : %d，expect : %d\n"};
+    for (struct FuncData *p = FuncData_head; p; p = p->next) {
+        printf("[%s, %s]\n", p->a_str, p->b_str);
+        struct TestFuncData data = {0, 0};
+        p->func(&data);
+        double rate = 1.0 * data.expect / data.total * 100;
+        int index = 0;
+        if (rate < 100) index = 1;
+        if (rate < 50) index = 2;
+        printf(color[index], rate, data.total, data.expect);
+    }
+    return 0;
 }
--- project3/test.h
+++ project3/test.h
@@ -13,29 +13,22 @@ struct TestFuncData{
     int total, expect;
 };
 
-typedef void (*test_func_t)();
+typedef void (*test_func_t)(struct TestFuncData *);
+
 struct FuncData {
     const char *a_str, *b_str;
     test_func_t func;
     struct FuncData *next;
 };
 
-struct FuncData *FuncData_head = NULL;
 
-struct FuncData *getFuncData(const char *a, const char *b, test_func_t func, struct FuncData *next) {
-    struct FuncData *p = (struct FuncData *)malloc(sizeof(struct FuncData));
-    p->a_str = a;
-    p->b_str = b;
-    p->func = func;
-    p->next = next;
-    return p;
-}
+void addFuncData(const char *a, const char *b, test_func_t func);
 
 #define TEST(a,b) \
     void a##_haizeix_##b(struct TestFuncData *); \
     __attribute__((constructor)) \
     void ADDFUNC_##a##_haizeix_##b() { \
-        FuncData_head = getFuncData(#a, #b, a##_haizeix_##b, FuncData_head); \
+        addFuncData(#a, #b, a##_haizeix_##b); \
     } \
     void a##_haizeix_##b(struct TestFuncData *__data)
 
@@ -47,34 +40,7 @@ struct FuncData *getFuncData(const char *a, const char *b, test_func_t func, str
 })
 
 
-int RUN_ALL_TEST() {
-    struct FuncData *current_node, *next_node;
-    if (FuncData_head == NULL) return 0;
-    current_node = FuncData_head->next;
-    FuncData_head->next = NULL;
-    while (current_node != NULL) {
-        next_node = current_node->next;
-        current_node->next = FuncData_head;
-        FuncData_head = current_node;
-        current_node = next_node;
-    }
-    char color[3][100] = {
-        "[ \033[1;32m%.2lf%%\033[0m ] total : %d，expect : %d\n",
-        "[ \033[0;31m%.2lf%%\033[0m ] total : %d，expect : %d\n",
-        "[ \033[1;31m%.2lf%%\033[0m ] total : %d，expect : %d\n"};
-    for (struct FuncData *p = FuncData_head; p; p = p->next) {
-        printf("[%s, %s]\n", p->a_str, p->b_str);
-        struct TestFuncData data = {0, 0};
-        p->func(&data);
-        double rate = 1.0 * data.expect / data.total * 100;
-        int index = 0;
-        if (rate < 100) index = 1;
-        if (rate < 50) index = 2;
-        printf(color[index], rate, data.total, data.expect);
-    }
-    return 0;
-}
-
+int RUN_ALL_TEST();
 
 
 #endif
