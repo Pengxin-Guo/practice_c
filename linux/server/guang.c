1a4e2ac89601b2ffc48572c4a162fb12987a1587
gpx <1457495424@qq.com>
2018-11-13 21:48:15 +0800

    mpdofy guang.c

--- linux/server/guang.c
+++ linux/server/guang.c
@@ -1,34 +1,37 @@
 /*************************************************************************
-	> File Name: guang.c
-	> Author: gpx
-	> Mail: 1457495424@qq.com
-	> Created Time: 2018年11月13日 星期二 19时55分32秒
- ************************************************************************/
+> File Name: guang.c
+> Author: gpx
+> Mail: 1457495424@qq.com
+> Created Time: 2018年11月13日 星期二 19时55分32秒
+************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "./head.h"
 
-void *my_fun(int n) {
-    printf("子线程id:%lu 遍历树莓派%d\n", pthread_self(), n);
+void *my_fun(void *arg) {
+    printf("子线程id:%lu 遍历树莓派\n", pthread_self());
+    pthread_exit(NULL);
     return NULL;
 }
 
 int main() {
     pthread_t pthid;
-    int res, i;
-    for (i = 1; i <= 5; i++) {
-        res = pthread_create(&pthid, NULL, my_fun(i), NULL);
-        printf("%lu***\n", pthid);
-        if(res != 0)
-        {
-            printf("%s***\n",strerror(res));   //打印错误信息
-            break;
+    int res, i, j = 1;
+    while (1) {
+        printf("第%d次遍历\n", j);
+        for (i = 1; i <= 5; i++) {
+            res = pthread_create(&pthid, NULL, my_fun, NULL);
+            if(res != 0)
+            {
+                printf("%s***\n",strerror(res));   //打印错误信息
+                break;
+            }
         }
-        printf("i : %d\n", i);
+        j++;
+        sleep(2);
+        //pthread_exit(NULL);            // 退出当前线程, 并没有结束整个进程
+
     }
-    printf("主线程id:%lu\n", pthread_self());
-    sleep(2);
-    //pthread_exit(NULL);            // 退出当前线程, 并没有结束整个进程
     return 0;
 }
